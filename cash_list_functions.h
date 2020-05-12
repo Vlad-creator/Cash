cc2a0ee91a6c26a23b5490c804134fe4d52cf333
Vlad-creator <56085984+Vlad-creator@users.noreply.github.com>
2020-05-12 23:32:02 +0300

    функции очереди

---
+++ cash_list_functions.h
@@ -0,0 +1,11 @@
+#pragma once
+
+struct node_t
+{
+	int age;
+	struct node_t* next;
+}
+
+struct node_t* push_list(struct node_t* last , int tag);
+struct node_t* create_node(int age);
+void Find_Min(struct node_t* head);
