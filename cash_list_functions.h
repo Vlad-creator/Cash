d58b2d0bf82e3fd47baff4eaa646e6939d984bbe
Vlad-creator <56085984+Vlad-creator@users.noreply.github.com>
2020-05-12 23:33:03 +0300

    Update cash_list_functions.h

--- cash_list_functions.h
+++ cash_list_functions.h
@@ -6,6 +6,7 @@ struct node_t
 	struct node_t* next;
 }
 
-struct node_t* push_list(struct node_t* last , int tag);
-struct node_t* create_node(int age);
-void Find_Min(struct node_t* head);
+struct node_t* push_list(struct node_t* last , int tag);//добавление эелемента в конец списка
+struct node_t* create_node(int age);//создание первой ячейки
+void Find_Min(struct node_t* head);//поиск эелемента с наименьшим приоритетом
+
