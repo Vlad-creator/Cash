424dffc95bc577d7a4f8bb8eaf355a344adc64fc
Vlad-creator <56085984+Vlad-creator@users.noreply.github.com>
2020-05-13 02:15:12 +0300

    Update cash_list_functions.h
    добавлены комменты

--- cash_list_functions.h
+++ cash_list_functions.h
@@ -8,7 +8,10 @@ struct node_t
 }
 
 struct node_t* push_list(struct node_t* last , int* data);//добавление эелемента в конец списка
+//int* data - адрес данных которые кладутся в node_t
 struct node_t* create_node(int* data);//создание первой ячейки
 void Find_Min(struct node_t* head , int new_data);//поиск эелемента с наименьшим приоритетом и замена на новые данные
+//int new_data - новые данные 
 void print_list(FILE *f, struct node_t *p)//вывод списка на экран
+//struct node_t* p - голова списка
 
