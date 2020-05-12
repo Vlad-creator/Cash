a2f6f25c168dd8002e4e31c31469616f5566b234
Vlad-creator <56085984+Vlad-creator@users.noreply.github.com>
2020-05-13 02:11:10 +0300

    Update cash_list_functions.h
    новая переменная в Find_Min
    добавление функции вывода списка

--- cash_list_functions.h
+++ cash_list_functions.h
@@ -9,5 +9,6 @@ struct node_t
 
 struct node_t* push_list(struct node_t* last , int* data);//добавление эелемента в конец списка
 struct node_t* create_node(int* data);//создание первой ячейки
-void Find_Min(struct node_t* head);//поиск эелемента с наименьшим приоритетом
+void Find_Min(struct node_t* head , int new_data);//поиск эелемента с наименьшим приоритетом и замена на новые данные
+void print_list(FILE *f, struct node_t *p)//вывод списка на экран
 
