6014c529150eef4323c3f2fa14ec218f803fc6de
Vlad-creator <56085984+Vlad-creator@users.noreply.github.com>
2020-05-13 01:58:36 +0300

    Update cash_list_functions.h
    теперь функции стали такими которыми они должны быть по идее(но сыровато)

--- cash_list_functions.h
+++ cash_list_functions.h
@@ -2,11 +2,12 @@
 
 struct node_t
 {
-	int age;
+	int age;//возраст данных
+	int* data;//указатель на данные в таблице
 	struct node_t* next;
 }
 
-struct node_t* push_list(struct node_t* last , int tag);//добавление эелемента в конец списка
-struct node_t* create_node(int age);//создание первой ячейки
+struct node_t* push_list(struct node_t* last , int* data);//добавление эелемента в конец списка
+struct node_t* create_node(int* data);//создание первой ячейки
 void Find_Min(struct node_t* head);//поиск эелемента с наименьшим приоритетом
 
