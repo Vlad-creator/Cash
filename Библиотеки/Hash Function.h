2614acbbed2f0efc4c1e674a5f930dce456a20fb
Nikita Valov <55804292+Noksis@users.noreply.github.com>
2020-05-15 03:35:18 +0300

    Переписал библиотеку функций

--- Библиотеки/Hash Function.h
+++ Библиотеки/Hash Function.h
@@ -1,15 +1,20 @@
-int NO_VALUE = 0;
+#pragma once
+#include "queue.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#pragma warning (disable:4996)
+
 
 struct list_d {
 	struct list_d* parent = NULL;
 	struct list_d* son = NULL;
-	int value = NO_VALUE;
-	int* ptr; //Указатель на твою структуру из частот
+	int value = 0;
 };
 
-struct list_d create_node(struct list_d parent);
+struct list_d create_node(struct list_d* parent);
 
-struct list_d del_node(struct list_d parent);
+struct list_d del_node(struct list_d* parent);
 
 int hash_f(int x, int cash_len);
 
@@ -17,4 +22,6 @@ void create_hash(int cash_len, struct list_d** hash_t);
 
 void hash_del(struct list_d* hash_t, int value, int cash_len);
 
-int add_hash(struct list_d* hash_t, int cash_len, int value);
+void add_hash(struct list_d* hash_t, int cash_len, int value);
+
+int check_in_hash(int value, struct list_d* hash_t, int cash_len);
