37b9a808aa8cd2e2642705478f837d4d22e91d18
Nikita Valov <55804292+Noksis@users.noreply.github.com>
2020-05-15 03:35:49 +0300

    Переписал библиотеку Влада

--- Библиотеки/queue.h
+++ Библиотеки/queue.h
@@ -1,9 +1,18 @@
+#pragma once
 struct node_t
 {
-	int age;
-	struct node_t* next;
-}
+	int age = 100;
+	int data = 0;
+	struct node_t* next = nullptr;
+	
+};
 
-struct node_t* push_list(struct node_t* last , int tag); //добавление элемента в конец списка
+struct node_t* push_list(struct node_t* last, int tag); //добавление элемента в конец списка
 struct node_t* create_node(int age); //создание первой ячейки списка
-void Find_Min(struct node_t* head); //нахождение ячейки с наименьшим приоритетом
+int Find_Min(struct node_t* head); //нахождение ячейки с наименьшим приоритетом
+
+
+// Написать фунцкию, которая находит частоту  данного значения и увеличивает на единицу ИЛИ ДОБАВЛЯЕТ ЕГО В СПИСОК!
+void Incr_freq(struct node_t* head, int value);
+
+// Переписать Find_Min, чтобы возваращала int  с минимальным значением
